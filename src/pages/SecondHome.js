6e93a258e06a3fdc08324f178cdc3d1be453139b
thaismachado31 <thaismachado31@gmail.com>
2022-06-07 21:05:57 -0300

    routes criadas

---
+++ src/pages/SecondHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SecondHome() {
+  return <div></div>;
+}
+
+export default SecondHome;
---
+++ src/pages/TaskDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TaskDetail() {
+  return <div></div>;
+}
+
+export default TaskDetail;
